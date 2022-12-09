385c76a5c30e487b306f96871f2d0ff7b3762c63
takuya shimizu <p.t.901.ts@gmail.com>
2022-12-09 14:48:05 +0900

    init

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,15 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.create(params.require(:contact).permit(:name, :email, :content))
+    if @contact.save
+      redirect_to new_contact_path, notice: "問い合わせを送信しました！"
+    else
+      render :new
+    end
+  end
+end
